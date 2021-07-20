3b8f51e8607f64b4b919df95807619533e0d504b
Masaki Tagawa <masaki@tagawa.email>
2021-07-20 21:45:05 +0900

    Use `pyproject.toml` as the source of truth for version
    https://github.com/python-poetry/poetry/pull/2366?w=1#issuecomment-652418094

--- pysesame3/__init__.py
+++ pysesame3/__init__.py
@@ -1,4 +1,8 @@
 """Top-level package for pysesame3."""
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata  # type: ignore
 
 __author__ = """Masaki Tagawa"""
-__version__ = "0.4.1"
+__version__ = importlib_metadata.version(__name__)
